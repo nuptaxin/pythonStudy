cad61e8ca1ba93e0d5ec6e432d30d0c78648673f
nuptaxin <nuptaxin@gmail.com>
2017-12-04 21:20:35 +0800

    第一天代码补充以及包名、类名更新

---
+++ 1_first_python/1_2_Output.py
@@ -0,0 +1,8 @@
+#单个字符串输出
+print('hello, world')
+#也可以使用双引号
+print("hello, world")
+#多个字符串用逗号隔开即可，打印是会自动加逗号
+print('hello','world')
+#打印整数或者计算结果
+print('100 + 200 =', 100 + 200)
---
+++ 1_first_python/1_3_Input.py
@@ -0,0 +1,6 @@
+#python提供了input()可以让用户输入字符串，并存放到一个变量中
+name = input()
+print('my name is', name)
+#input()可以显示一个字符串来提示用户输入
+name = input('please enter your name: ')
+print('your name is :', name)
